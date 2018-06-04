4bed03657fbbcb29bad0f61306b78162d934ac46
Guillaume Laubier <glaubier@smartadserver.com>
2018-06-04 14:01:12 +0200

    create SudokuGrid struct + can display grid correctly

---
+++ SudokuResolver.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type SudokuGrid struct {
+  grid [9][9]int
+}
+
+func main() {
+  grid := SudokuGrid{grid: [9][9]int{}}
+  displayGrid(grid)
+}
+
+
+func displayGrid(sudokuGrid SudokuGrid) {
+  fmt.Println("--------------------------")
+
+  for i := 0; i < 9; i += 3 {
+    for j := 0; j < 9; j += 3 {
+      fmt.Println("|", sudokuGrid.grid[i][j], sudokuGrid.grid[i][j + 2], sudokuGrid.grid[i][j + 2], "|", sudokuGrid.grid[i + 1][j], sudokuGrid.grid[i + 1][j + 2], sudokuGrid.grid[i + 1][j + 2], "|", sudokuGrid.grid[i + 2][j], sudokuGrid.grid[i + 2][j + 2], sudokuGrid.grid[i + 2][j + 2], "|")
+    }
+    
+    fmt.Println("--------------------------")
+  }
+}
